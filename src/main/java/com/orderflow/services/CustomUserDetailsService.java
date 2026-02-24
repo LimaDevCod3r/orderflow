82e438bceba0e1a5408447c4bb6a55b8f88a1b24
Diego Lima <diegolima20100@gmail.com>
2026-02-24 19:28:38 -0300

    feat: integra UserDetails e UserDetailsService ao fluxo de autenticação

---
+++ src/main/java/com/orderflow/services/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.orderflow.services;
+
+import com.orderflow.config.CustomUserDetails;
+import com.orderflow.domain.entities.User;
+import com.orderflow.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
