240f96172060f2a736f37f2b308ee41c2bf21b2a
Diego Lima <diegolima20100@gmail.com>
2026-02-23 21:49:50 -0300

    feat: adiciona configuração inicial do Spring Security

---
+++ src/main/java/com/orderflow/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.orderflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() // Rota de auth aberta
+                        .anyRequest().authenticated() // Todo o resto bloqueado
+                )
+                // Aqui entrará o filtro de JWT que explicarei abaixo
+                .build();
+    }
+
+
+    // criptografar a senha
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
